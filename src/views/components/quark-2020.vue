04e1e6179f3c59cf69b3d9534e461ed485fcdab2
2017A7PS0125G <f20170125@goa.bits-pilani.ac.in>
2019-07-18 19:01:32 +0530

    Added a basic Vue.cli template to work on

---
+++ src/views/components/quark-2020.vue
@@ -0,0 +1,18 @@
+<template></template>
+<script>
+function imagify(colour) {
+  var out =
+    `<img src="img/brand/quark_` +
+    colour +
+    `white.png" id="white_png" width="400px" class="img-fluid" />`;
+  return out;
+}
+
+export default {
+  name: "quark",
+  data() {
+    return {};
+  }
+};
+</script>
+
